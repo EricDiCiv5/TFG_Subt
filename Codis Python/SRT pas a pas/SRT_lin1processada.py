c2c1ddddd8e3535fbe549d3ca6f5bb09a5ea6302
EDiaz <heimricusdc@gmail.com>
2018-01-18 19:06:58 +0100

    Codi Pas 2 actualitzat

--- Codis Python/SRT pas a pas/SRT_lin1processada.py
+++ Codis Python/SRT pas a pas/SRT_lin1processada.py
@@ -4,8 +4,6 @@ i printar per pantalla la sublínia més el número de la seva longitud."""
 import os 
 import sys 
 
-num_subt = 1 #Contador del número de subtítol
-
 #Obro fitxers de text de lectura
 f1 = open('002_frases.txt', 'r') 
 
@@ -13,20 +11,22 @@ for lin1 in f1:
 
   while len(lin1):
 
-    lin1_35 = lin1[0:35] 
+    if(len(lin1) > 35):
 
-    subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat.
+      lin1_35 = lin1[0:35] 
 
-    print(subf," (longitud = ",len(subf),")") #Imprimeixo subfrase i la seva longitud
+      subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat.
 
-    lin1 = lin1[lin1_35.rindex(' ')+1:] #Actualitzo valor resultant de línia.
+      print(subf," (longitud = ",len(subf),")") #Imprimeixo subfrase i la seva longitud
 
-    if (len(lin1) < 35):
+      lin1 = lin1[lin1_35.rindex(' ')+1:] #Actualitzo valor resultant de línia.
 
-      lin1_x = lin1[0:end]
+    else:
 
-      subf = lin1_x[:lin1_x.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat.
+      lin1_x = lin1[0:]
 
-      print(subf," (longitud = ",len(subf),")") #Imprimeixo subfrase i la seva longitud.
+      subf = lin1_x[:lin1_x.rindex('\n')] #Variable 'subf' serà la subfrase fins al darrer espai trobat.
+
+      print(subf," ( longitud = ",len(subf),")") #Imprimeixo subfrase i la seva longitud.
 
 f1.close()
