34a04967195d8c70a9a23aac53898deb5e8dbda1
EDiaz <heimricusdc@gmail.com>
2018-02-06 12:19:31 +0100

    Update SRT_marquestemps.py

--- Codis Python/SRT pas a pas/SRT_marquestemps.py
+++ Codis Python/SRT pas a pas/SRT_marquestemps.py
@@ -8,19 +8,21 @@ import datetime
 marca_ini = 0.00
 marca_fin = 0.00
 
-with open('002_frases.txt', 'r') as fT, open('arxiu73_words_002.lab') as fL:
+#Obro fitxers de text de lectura
+fT = open('002_frases.txt', 'r')
+fL = open('arxiu73_words_002.lab', 'r')
 
-  for lin1, lin2 in zip(fT, fL):
+for lin2 in fL:
+
+  for lin1 in fL:
 
     while len(lin1):
 
       if(len(lin1) > 35):
 
         lin1_35 = lin1[0:35] 
-
-        subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat.
-
-        par_begin = subf[0:subf.rindex(' ')] #Agafo la primera paraula de la subfrase. 
+ 
+        subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat. 
 
         print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
 
@@ -30,17 +32,20 @@ with open('002_frases.txt', 'r') as fT, open('arxiu73_words_002.lab') as fL:
 
         subf = lin1[0:]
 
-        print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.. 
+        par_begin = subf
+
+        print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud. 
       
         lin1 = [] #Actualitzo valor resultant de línia.
 
-        i_time,f_time,word = lin2.split() #Parteixo línia en tres paràmetres: marca inicial, marca final i la paraula
+  i_time,f_time,word = lin2.split() #Parteixo línia en tres paràmetres: marca inicial, marca final i la paraula.
 
-        if(word == subf):
+  if(word == subf[0]):
 
-          marca_ini = i_time
+    marca_ini = i_time
 
-          print(marca_ini) 
+    print(marca_ini) 
+
 
 fT.close()
 fL.close()
