da77bb4be81848b5150df8e1509d9ae70b0d2879
EDiaz <heimricusdc@gmail.com>
2018-02-12 11:40:49 +0100

    Nou esborrany d'aquest codi

--- Codis Python/SRT pas a pas/SRT_marquestemps.py
+++ Codis Python/SRT pas a pas/SRT_marquestemps.py
@@ -12,40 +12,47 @@ marca_fin = 0.00
 fT = open('002_frases.txt', 'r')
 fL = open('arxiu73_words_002.lab', 'r')
 
-for lin2 in fL:
 
-  for lin1 in fL:
+for lin1 in fT:
 
-    while len(lin1):
+  while len(lin1):
 
-      if(len(lin1) > 35):
+    if(len(lin1) > 35):
 
-        lin1_35 = lin1[0:35] 
+      lin1_35 = lin1[0:35] 
  
-        subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat. 
+      subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat.
+
+      for lin2 in fL.readlines():
+
+        i_time,f_time,word = lin2.split() #Parteixo línia en tres paràmetres: marca inicial, marca final i la paraula.
 
         print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
 
-        lin1 = lin1[lin1_35.rindex(' ')+1:] #Actualitzo valor resultant de línia.
+        if(word == subf[0]):
+        
+          marca_ini = i_time
 
-      else:
+          ini_t = str(datetime.timedelta(marca_ini))
 
-        subf = lin1[0:]
+        if(word == subf[-1]):
 
-        par_begin = subf
+          marca_fin = f_time
 
-        print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud. 
-      
-        lin1 = [] #Actualitzo valor resultant de línia.
+          fin_t = str(datetime.timedelta(marca_fin))
 
-  i_time,f_time,word = lin2.split() #Parteixo línia en tres paràmetres: marca inicial, marca final i la paraula.
+        print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
 
-  if(word == subf[0]):
+      lin1 = lin1[lin1_35.rindex(' ')+1:] #Actualitzo valor resultant de línia.
 
-    marca_ini = i_time
+    else:
 
-    print(marca_ini) 
-
+      subf = lin1[0:]
+  
+      print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
+  
+      lin1 = [] #Actualitzo valor resultant de línia.
 
 fT.close()
 fL.close()
+
