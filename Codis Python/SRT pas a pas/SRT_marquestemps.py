3f90e7b22287d577d28a0b071bc229c1757e977f
EDiaz <heimricusdc@gmail.com>
2018-02-13 11:15:20 +0100

    marquestemps.py printant sortida
    encara no m'agafa marques del fitxer .LAB

--- Codis Python/SRT pas a pas/SRT_marquestemps.py
+++ Codis Python/SRT pas a pas/SRT_marquestemps.py
@@ -8,6 +8,9 @@ import datetime
 marca_ini = 0.00
 marca_fin = 0.00
 
+#Variable número de subtítol
+num_subt = 1
+
 #Obro fitxers de text de lectura
 fT = open('002_frases.txt', 'r')
 fL = open('arxiu73_words_002.lab', 'r')
@@ -21,38 +24,55 @@ for lin1 in fT:
 
       lin1_35 = lin1[0:35] 
  
-      subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat.
+      subf = lin1_35[:lin1_35.rindex(' ')] #Variable 'subf' serà la subfrase fins al darrer espai trobat. 
 
-      for lin2 in fL.readlines():
-
+      for lin2 in fL:
+        
         i_time,f_time,word = lin2.split() #Parteixo línia en tres paràmetres: marca inicial, marca final i la paraula.
-
-        print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
 
         if(word == subf[0]):
         
           marca_ini = i_time
 
-          ini_t = str(datetime.timedelta(marca_ini))
+        if(word == subf[-1]):
+
+          marca_fin = f_time
+
+      print(num_subt)
+
+      print(marca_ini," --> ",marca_fin)
+
+      print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
+
+      lin1 = lin1[lin1_35.rindex(' ')+1:] #Actualitzo valor resultant de línia.
+
+      num_subt += 1
+
+    else:
+
+      subf = lin1[0:]
+
+      for lin2 in fL:
+        
+        i_time,f_time,word = lin2.split() #Parteixo línia en tres paràmetres: marca inicial, marca final i la paraula.
+
+        if(word == subf[0]):
+        
+          marca_ini = i_time
 
         if(word == subf[-1]):
 
           marca_fin = f_time
 
-          fin_t = str(datetime.timedelta(marca_fin))
+      print(num_subt)
 
-        print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
+      print(marca_ini," --> ",marca_fin)
 
-      lin1 = lin1[lin1_35.rindex(' ')+1:] #Actualitzo valor resultant de línia.
-
-    else:
-
-      subf = lin1[0:]
-  
       print(len(subf),"/",len(lin1),":",subf) #Imprimeixo subfrase i la seva longitud.
-  
+
+      num_subt += 1
+
       lin1 = [] #Actualitzo valor resultant de línia.
 
 fT.close()
 fL.close()
-
