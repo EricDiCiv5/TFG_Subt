45d43fbe4eeeb86a35290d0af0fd753832e05f16
EDiaz <heimricusdc@gmail.com>
2018-03-01 19:19:31 +0100

    Update SRT_v7.py
    Agafa la subfrase com a llista però no la junta per espais.

--- Codis Python/SRT pas a pas/SRT_v7.py
+++ Codis Python/SRT pas a pas/SRT_v7.py
@@ -10,50 +10,59 @@ ResList = [] #Llista resultant
 
 AuxList = []*3 #Llista on es fa el processat de la subfrase
 
-Linia = []
+Linia = []#Llista on s'agafa cada linia del fitxer
 
 num_subt = 0
 
 for lin in fLab:
 
-  lin = lin.strip()
-
-  Linia = lin.split(' ')
+  Linia = lin.split(' ') #Parteixo linia en els 3 paràmetres que la formen
 
   if(Linia[2].isupper()): 
 
-    if(len(AuxList) != 0):
+    if(len(AuxList) != 0): #Si llista Auxiliar és plena...
 
       ResList.append(AuxList)
       
-      AuxList = [] 
+      AuxList = [] #Buidar llista Auxiliar
 
     ResList.append(Linia)
 
 
   else:
 
-    if(len(AuxList) == 0):
+    if(len(AuxList) == 0): #Si llista Auxiliar és buida...
 
-      AuxList = Linia[:]
+      AuxList = Linia[:] #Copio contingut llista línia en Auxiliar
 
     else:
 
-      if(len(AuxList[2] + Linia[2]) <= 35):
+      if(len(AuxList[2] + Linia[2]) <= 35): #Si longitud subfrase més paraules es inferior a 35...
 
-        AuxList[2] = AuxList[2] + Linia[2] 
+        AuxList[2] = AuxList[2] + Linia[2] #Juntar els strings en llista auxiliar
 
-        AuxList[1] = Linia[1]     
+        AuxList[1] = Linia[1] #Agafar marca de temps final     
 
       else:
 
         ResList.append(AuxList)
 
-        AuxList = Linia[:]
+        AuxList = Linia[:] #Copio contingut llista línia en Auxiliar
+
+
 
 while num_subt < len(ResList):
 
-  print(ResList[num_subt][0]," ",ResList[num_subt][1]," ",ResList[num_subt][2])
+  MeuString = ResList[num_subt][2].split()
+
+  ' '.join(MeuString)
+
+  ResList[num_subt][2] = MeuString
+
+  print(str(num_subt+1))
+  print(ResList[num_subt][0]," --> ",ResList[num_subt][1])
+  print(ResList[num_subt][2])
+  print('')
 
   num_subt += 1
 
