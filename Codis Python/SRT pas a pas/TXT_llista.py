c1cba56412a007723d9f80d3ba602f1a915cd7ec
EDiaz <heimricusdc@gmail.com>
2018-04-16 17:52:15 +0200

    Llista .txt per 100 fitxers
    Visualització en fitxers sortida SRT només d'exemple per comprobar que funciona

--- Codis Python/SRT pas a pas/TXT_llista.py
+++ Codis Python/SRT pas a pas/TXT_llista.py
@@ -6,36 +6,48 @@ import sys
 import glob
 
 #Fitxer de lectura
-fTXT = open("002_frases.txt", 'r')
+Conj_fTXTs = glob.glob('./*.txt')
 
-#Variables
-Subf = []
-
-
-#Fitxers .txt en llista
-for line in fTXT:
     
-  if not (line.split(' ')[0].isupper()) and not (line.split(' ')[0].islower()) or (line.split(' ')[0].islower()):
+#Fitxers .txt en llista
+for nom_fitx in Conj_fTXTs:
+  fTXT = open(nom_fitx, 'r')
+  fList = open(nom_fitx.replace('txt', 'srt'), 'w')
 
-    while len(line):
+  #Variables
+  Subf = []
 
-      if (len(line) > 35):
-        subf_35 = line[0:35]
-        subf = subf_35[:subf_35.rindex(' ')]
-        Subf.append(subf)
-        line = line[subf_35.rindex(' ')+1:]
+  for line in fTXT:
+
+    if not (line.split(' ')[0].isupper()) and not (line.split(' ')[0].islower()) or (line.split(' ')[0].islower()):
+
+      while len(line):
+
+        if (len(line) > 35):
+          subf_35 = line[0:35]
+          subf = subf_35[:subf_35.rindex(' ')]
+          Subf.append(subf)
+          line = line[subf_35.rindex(' ')+1:]
+          
    
-      else:
+        else:
       
-        subf_35 = line[0:]
-        subf = subf_35[:subf_35.rindex('\n')]
-        Subf.append(subf)
-        line = []
+          subf_35 = line[0:]
+          subf = subf_35[:subf_35.rindex('\n')]
+          Subf.append(subf)
+          line = []
+          
+ 
+  #Eliminar strings buits
+  Subf = list(filter(None, Subf))
 
-#Eliminar strings buits
-Subf = list(filter(None, Subf))
+  pos = 1
 
-#Visualització llista
-print(Subf)
+  #Visualització llista
+  while pos  < len(Subf):
+    fList.write(Subf[pos]+'\n')
+    fList.write('')
+    pos += 1
 
 fTXT.close()
+fList.close()
